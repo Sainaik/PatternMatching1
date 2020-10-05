6e003149b98517d7184e0f89f35ca72e5ec83ecc
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-05 09:25:36 +0530

    [Sai]Added. Pattern check for 6digit PIN no

--- PatternMatching1/Program.cs
+++ PatternMatching1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace PatternMatching1
 {
@@ -7,6 +8,26 @@ namespace PatternMatching1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Pattern Matching Problem");
+
+            
+
+            String Pattern = "^[1-9]{1}[0-9]{5}$";
+
+            Regex r = new Regex(Pattern);
+
+            String pinNo = Console.ReadLine();
+
+            if(r.IsMatch(pinNo))
+            {
+                Console.WriteLine("Valid PIN number");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid PIN number");
+
+            }
+        
         }
     }
 }
